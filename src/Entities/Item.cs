76524d20c9b4e170e53c8e2f994df0d83889c467
nicolRB <134354240+nicolRB@users.noreply.github.com>
2025-10-16 02:58:10 -0300

    Add files via upload

---
+++ src/Entities/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace RPGProject.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string Effect { get; set; } = string.Empty;
+        public int Power { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}

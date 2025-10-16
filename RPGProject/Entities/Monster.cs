978f35a3d0c9887c1abb05798f15bb720302988c
nicolRB <134354240+nicolRB@users.noreply.github.com>
2025-10-16 02:54:11 -0300

    Add files via upload

---
+++ RPGProject/Entities/Monster.cs
@@ -0,0 +1,14 @@
+﻿namespace RPGProject.Entities
+{
+    public class Monster
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Damage { get; set; }
+        public int Defense { get; set; }
+        public int Range { get; set; }
+        public int Speed { get; set; }
+    }
+}
---
+++ RPGProject/Entities/Weapon.cs
@@ -0,0 +1,13 @@
+﻿namespace RPGProject.Entities
+{
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Attack { get; set; }
+        public int Damage { get; set; }
+        public int Range { get; set; }
+        public int Speed { get; set; }
+        public string Type { get; set; } = "";
+    }
+}

6b6a83bc6a2d0d9da309cc829c84871be618107d
Nicolas <nicolasroborges@gmail.com>
2025-10-17 17:56:46 -0300

    Program adicionado ao git

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main()
     {
-        Console.Title = "RPG Project - Alpha 0.5.7";
+        Console.Title = "RPG Project - Alpha 0.3";
 
         // Executa o menu principal
         Menu.Show();
